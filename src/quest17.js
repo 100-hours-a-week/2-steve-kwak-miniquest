86f6a032a6f24a13f0443cd1c9f072b34dba2f86
dyk000825 <kdy000825@naver.com>
2025-02-23 18:19:11 +0900

    feat : quest 17 add

---
+++ src/quest17.js
@@ -0,0 +1,8 @@
+let temperature = 25;
+temperature = 30;
+console.log(temperature);
+
+const MAX_TEMPERATURE = 35;
+/* MAX_TEMPERATURE = 40; -> 여기서 오류 발생
+ let은 재할당 가능하기 떄문에 오류가 안나지만 const로 선언된 변수에는 재할당이 불가하여 오류 뜸.
+ */
