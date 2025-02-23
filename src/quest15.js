489558417e7456766d2bf921e1473285cb8f043a
dyk000825 <kdy000825@naver.com>
2025-02-23 18:09:31 +0900

    feat : quest 14 add

---
+++ src/quest15.js
@@ -0,0 +1,12 @@
+const message = 'Hello, JavaScript!';
+
+const showMessage = () => {
+    console.log(message); // A
+    let message = 'Hello, ES6!';
+    console.log(message); // B
+};
+
+showMessage();
+/* 예측 : error가 뜰거 같음
+   실행결과 : ReferenceError가 발생. message가 초기화되기 전에 접근할려고 시도했기 떄문.
+* */
